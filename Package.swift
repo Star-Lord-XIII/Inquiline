79ba1b86251f32f967858f42b3168cbaa099c13b
Chintan Ghate <chintanghate@gmail.com>
2023-07-21 20:54:55 +0100

    Fix Packange.swift & migrate to swift-version 5.8

--- Package.swift
+++ Package.swift
@@ -1,9 +1,21 @@
+// swift-tools-version: 5.8
+
 import PackageDescription
 
 let package = Package(
   name: "Inquiline",
+  products: [
+    .library(name: "Inquiline", targets: ["Inquiline"])
+  ],
   dependencies: [
-    .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 4),
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
+    .package(url: "https://github.com/Star-Lord-XIII/Nest", from: "0.5.0"),
+    .package(url: "https://github.com/kylef/Spectre.git", from: "0.7.0"),
+  ],
+  targets: [
+    .target(name: "Inquiline", dependencies: [
+        "Nest",
+        "Spectre",
+    ],
+    path: "Sources")
   ]
 )
